631ab593d81e9e2a7defc016df4ce88b4bc3cb2d
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-01-10 15:54:48 +0800

    added some forms

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
 from src import app
-from flask import render_template
+from flask import render_template, redirect, url_for
+from src.forms import RegistrationForm, LoginForm
 
 
 class Box:
@@ -10,12 +11,43 @@ class Box:
         self.description = description
 
 
-BOXES = [Box("Portfolio"), Box("Contants"), Box("Game"), Box("Services")]
+BOXES = [Box("Portfolio"), Box("Contacts"), Box("Game"), Box("Services")]
 
 
 @app.route("/")
 @app.route("/home/")
 def home():
+    return render_template("home.html", boxes=BOXES)
+
+
+@app.route("/login/")
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email == "test@test.com" and form.password == "password":
+            return redirect(url_for('home'))
+
+    return render_template("login.html", form=form)
+
+
+@app.route("/logout/")
+def logout():
+    return render_template("home.html", boxes=BOXES)
+
+
+@app.route("/register/", methods=["POST", "GET"])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+
+    return render_template("register.html", form=form)
+
+
+@app.route("/account/")
+def account():
     return render_template("home.html", boxes=BOXES)
 
 
