3b1d78d77b6631dd494f9ba1a8b27243a0dac946
federico101 <fredericktulba9@gmail.com>
2022-01-14 14:34:06 +0800

    created navbar(homepage)

--- src/routes.py
+++ src/routes.py
@@ -1,25 +1,14 @@
 from src import app
 from flask import render_template
 
-
-class Box:
-    def __init__(self, name, description=None) -> None:
-        self.name = name
-        if description is None:
-            description = f"{name} related stuff"
-        self.description = description
-
-BOXES = [Box("Discord"), Box("Game"), Box("Portfolio"), Box("Services")]
-
-
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", boxes=BOXES)
+    return render_template("home.html")
 
 @app.route("/admin")
 def admin():
-    return render_template("home.html", boxes=BOXES)
+    return render_template("home.html")
 
 @app.route("/discord")
 def discord():
@@ -27,12 +16,12 @@ def discord():
 
 @app.route("/game")
 def game():
-    return render_template("home.html", boxes=BOXES)
+    return render_template("home.html")
 
 @app.route("/portfolio")
 def portfolio():
-    return render_template("home.html", boxes=BOXES)
+    return render_template("home.html")
 
 @app.route("/services")
 def services():
-    return render_template("home.html", boxes=BOXES)
+    return render_template("home.html")
