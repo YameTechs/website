3956882bc23d6c8d6baa3e127077f48785e308fc
Cpaul777 <sabanalpaul009@gmail.com>
2022-01-30 17:45:26 +0800

    Pc UI adjusted

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -1,10 +1,10 @@
 // toggle burgir menu //
 
-const burgirBtn = document.getElementsByClassName('burgir')[0]
-const menuShow = document.getElementsByClassName('menu')[0]
+const burgirBtn = document.getElementsByClassName("burgir")[0];
+const menuShow = document.getElementsByClassName("menu")[0];
 
-burgirBtn.addEventListener('click', () => {
-  menuShow.classList.toggle('active')
-})
+burgirBtn.addEventListener("click", () => {
+  menuShow.classList.toggle("active");
+});
 
-//end gurgir //
+//end burgir //
