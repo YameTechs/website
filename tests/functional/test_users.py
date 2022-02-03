8c5a23ccf67141d64ff2635705bc525efc0a8448
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-02-03 13:12:34 +0800

    add simple test

--- tests/functional/test_users.py
+++ tests/functional/test_users.py
@@ -18,3 +18,33 @@ def test_home_page_post(test_client):
     """
     response = test_client.post("/")
     assert response.status_code == 405
+
+
+def test_register(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/register' page is requested (GET)
+    THEN check that the response is valid
+    """
+    response = test_client.get("/register/")
+    assert response.status_code == 200
+    assert b"register" in response.data
+
+    assert b"username" in response.data
+    assert b"email" in response.data
+    assert b"password" in response.data
+    assert b"confirm" in response.data
+
+
+def test_login(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' page is requested (GET)
+    THEN check that the response is valid
+    """
+    response = test_client.get("/login/")
+    assert response.status_code == 200
+    assert b"login" in response.data
+
+    assert b"email" in response.data
+    assert b"password" in response.data
