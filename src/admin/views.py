5ca7eb6b01c16fbecc9f606be11baffbfebe57b4
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-02-09 15:29:09 +0800

    nani?!

--- src/admin/views.py
+++ src/admin/views.py
@@ -1,13 +1,13 @@
 from flask import abort
-from flask_admin.contrib.sqla import ModelView
 from flask_admin import AdminIndexView
+from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 
 
 class MyAdminView(ModelView):
     def is_accessible(self):
         # The order of the if statements is important
-        return current_user.is_authenticated and current_user.has_role('admin')
+        return current_user.is_authenticated and current_user.has_role("admin")
 
     def inaccessible_callback(self, name, **kwargs):
         return abort(404)
@@ -15,7 +15,7 @@ class MyAdminView(ModelView):
 
 class MyAdminIndexView(AdminIndexView):
     def is_accessible(self):
-        return current_user.is_authenticated and current_user.has_role('admin')
+        return current_user.is_authenticated and current_user.has_role("admin")
 
     def inaccessible_callback(self, name, **kwargs):
         return abort(404)
