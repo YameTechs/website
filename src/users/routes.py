a14372b95016a68a1b1a4a64bec0e713f3681e54
federico101 <fredericktulba9@gmail.com>
2022-02-10 15:24:14 +0800

    tryin to fix merge

--- src/users/routes.py
+++ src/users/routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
-from src import bcrypt, db
-from src.models import User
+from src import _bcrypt, _db
+from src.models import Role, User
 from src.users.forms import (
     LoginForm,
     RegistrationForm,
@@ -11,6 +11,7 @@ from src.users.forms import (
     ResetPasswordForm,
 )
 from src.users.utils import send_user_email
+from src.utils import logout_required
 
 users = Blueprint("users", __name__)
 
@@ -20,69 +21,61 @@ users = Blueprint("users", __name__)
 def account():
     form = ResendEmailButton()
 
-    if form.validate_on_submit():
-        if form.submit.data:
-            msg_body = (
-                "To verify your account, visit the following link:\n"
-                "{}\n"
-                "If you did not make this request then simply ignore this email and no"
-                "changes will be made"
-            )
-            send_user_email(
-                current_user, "Account Verification", msg_body, "users.verify_token"
-            )
-            flash("An email has been send with your verification link!")
+    if not (form.validate_on_submit() and form.submit.data):
+        return render_template("account.html", getattr=getattr, form=form)
+
+    msg_body = (
+        "To verify your account, visit the following link:\n"
+        "{}\n"
+        "If you did not make this request then simply ignore this email and no "
+        "changes will be made"
+    )
+
+    send_user_email(
+        current_user, "Account Verification", msg_body, "users.verify_token"
+    )
+
+    flash("An email has been send with your verification link!")
 
     return render_template("account.html", getattr=getattr, form=form)
 
 
 @users.route("/login/", methods=["POST", "GET"])
+@logout_required
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for("main.home"))
-
     form = LoginForm()
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+    if not form.validate_on_submit():
+        return render_template("login.html", form=form)
 
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get("next")
-            flash('Logged in!')
-            return redirect(next_page or url_for("main.home"))
-        else:
-            flash("incorrect data!")
+    user = User.query.filter_by(email=form.email.data).first()
+    if user and _bcrypt.check_password_hash(user.password, form.password.data):
+        login_user(user, remember=form.remember.data)
+        next_page = request.args.get("next")
+        return redirect(next_page or url_for("main.home"))
 
+    flash("incorrect data!")
     return render_template("login.html", form=form)
 
 
 @users.route("/logout/")
 def logout():
     logout_user()
-    flash('Logged out!')
     return redirect(url_for("main.home"))
 
 
 @users.route("/register/", methods=["POST", "GET"])
+@logout_required
 def register():
-    print("enter register")
-    if current_user.is_authenticated:
-        print("user is authenticated")
-        return redirect(url_for("main.home"))
-
     form = RegistrationForm()
-    print("form is valid")
     if not form.validate_on_submit():
-        print("returned")
         return render_template("register.html", form=form)
 
-    hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+    hashed_password = _bcrypt.generate_password_hash(form.password.data).decode("utf-8")
     user = User(
         email=form.email.data,
         username=form.username.data,
         password=hashed_password,
-        is_verified=False,
     )
 
     msg_body = (
@@ -93,78 +86,69 @@ def register():
     )
 
     send_user_email(user, "Account Verification", msg_body, "users.verify_token")
-    print("email sent")
 
-    db.session.add(user)
-    db.session.commit()
-    print("added in database")
+    _db.session.add(user)
+    _db.session.commit()
 
-    print("you have been registered")
     flash("You have registered in!")
     return redirect(url_for("users.login"))
 
 
 @users.route("/request_token/", methods=["POST", "GET"])
+@logout_required
 def request_token():
-    if current_user.is_authenticated:
-        return redirect(url_for("main.home"))
-
     form = RequestResetFrom()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None:
-            flash("that email seems to be invalid!")
-            return redirect(url_for("users.request_token"))
+    if not form.validate_on_submit():
+        return render_template("request_token.html", form=form)
 
-        msg_body = (
-            "To change your password, visit the following link:\n"
-            "{}\n\n"
-            "If you did not make this request then simply ignore this email and no"
-            "changes will be made"
-        )
+    user = User.query.filter_by(email=form.email.data).first()
+    if user is None:
+        flash("that email seems to be invalid!")
+        return redirect(url_for("users.request_token"))
 
-        send_user_email(user, "Forgotten Password", msg_body, "users.reset_password")
-        flash("an email has been sent!")
-        return redirect(url_for("users.login"))
-    return render_template("request_token.html", form=form)
+    msg_body = (
+        "To change your password, visit the following link:\n"
+        "{}\n\n"
+        "If you did not make this request then simply ignore this email and no"
+        "changes will be made"
+    )
+
+    send_user_email(user, "Forgotten Password", msg_body, "users.reset_password")
+    flash("an email has been sent!")
+    return redirect(url_for("users.login"))
 
 
 @users.route("/reset_password/<token>", methods=["GET", "POST"])
+@logout_required
 def reset_password(token):
-    if current_user.is_authenticated:
-        return redirect(url_for("home"))
-
     user = User.get_user_by_token(token)
     if user is None:
         flash("That is an invalid or expired token")
         return redirect(url_for("users.request_token"))
 
     form = ResetPasswordForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
-            "utf-8"
-        )
-        user.password = hashed_password
-        db.session.commit()
-        flash("Your password has been updated! You are now able to log in", "success")
-        return redirect(url_for("users.login"))
-    return render_template("reset_password.html", title="Reset Password", form=form)
+    if not form.validate_on_submit():
+        return render_template("reset_password.html", title="Reset Password", form=form)
+
+    user.password = _bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+    _db.session.commit()
+    flash("Your password has been updated! You are now able to log in", "success")
+    return redirect(url_for("users.login"))
+
+
+@users.route("/settings/", methods=["POST", "GET"])
+@login_required
+def settings():
+    return render_template("settings.html")
 
 
 @users.route("/verify_token/<token>/", methods=["POST", "GET"])
 def verify_token(token):
     user = User.get_user_by_token(token)
     if user is None:
-        msg = "that's an invalid token lul, please request a new verification token"
-        return render_template("verify_token.html", msg=msg)
+        return render_template("verify_token.html", success=False)
 
-    user.is_verified = True
-    db.session.commit()
-    msg = "Congratulation you have been verified"
-    return render_template("verify_token.html", msg=msg)
-
-
-@users.route("/services/")
-def about():
-    about()
-    return redirect(url_for("main.home"))
+    verified_role = Role.query.filter_by(name="verified").first()
+    user.roles.append(verified_role)
+    _db.session.commit()
+    return render_template("verify_token.html", success=True)
