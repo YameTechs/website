8a58b67c2d711e1bab50b569bc040982bc36da96
m0nk3ybraindead <zyphr.euch@gmail.com>
2022-02-11 08:58:05 +0800

    cleaned routes.py and applied formats

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
--- src/admin/views.py
+++ src/admin/views.py
@@ -1,5 +1,5 @@
-from flask import abort
-from flask_admin import AdminIndexView
+from flask import Blueprint, abort
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 
@@ -19,3 +19,28 @@ class MyAdminIndexView(AdminIndexView):
 
     def inaccessible_callback(self, name, **kwargs):
         return abort(404)
+
+
+# A class to make flask_admin work with flask Blueprints
+class AdminBlueprint(Blueprint):
+    views = []
+
+    def __init__(self, *args, **kwargs):
+        super(AdminBlueprint, self).__init__(*args, **kwargs)
+
+        # Create admin
+        self._admin = Admin(
+            name="Database",
+            index_view=MyAdminIndexView(url=f"{self.url_prefix}database"),
+        )
+
+    def add_view(self, view):
+        self.views.append(view)
+
+    def register(self, app, options):
+        self._admin.init_app(app)
+
+        for view in self.views:
+            self._admin.add_view(view)
+
+        return super(AdminBlueprint, self).register(app, options)
