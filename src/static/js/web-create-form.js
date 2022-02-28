a5c2c73ac15e83fa596b5be2662ffb6004b05ec8
Cpaul777 <sabanalpaul009@gmail.com>
2022-02-28 20:46:44 +0800

    fixed forms and dc form

--- src/static/js/web-create-form.js
+++ src/static/js/web-create-form.js
@@ -3,32 +3,50 @@
 //the Overlay background layer
 const overlay = document.getElementsByClassName("outside-layer")[0];
 
-// the pop up/modal
-const modalOpen = document.getElementsByClassName("theModal")[0];
+// Website Modal
+const webModal = document.getElementById("web-form");
+
+// Discord Modal
+const dcModal = document.getElementById("web-form2");
 
 //website button option
 const websiteButton = document.getElementById("web-button");
 
 //discord button
-//const
+const discordButton = document.getElementById("dc-btn");
 
 //The close button
 const closeModal = document.getElementById("close");
+const closeModal2 = document.getElementById("close2");
 
-// Function for when clicking the button it will show the form
+// Function for when clicking the Website button it will show the form
 websiteButton.addEventListener("click", () => {
   overlay.classList.toggle("overlay");
-  modalOpen.classList.toggle("active");
+  webModal.classList.toggle("active");
+  closeModal.classList.add("active");
 });
 
-//x button when pressed
+// Function for when clicking the Discord Button
+discordButton.addEventListener("click", () => {
+  overlay.classList.toggle("overlay");
+  dcModal.classList.toggle("active");
+});
+
+// x button (Web) when pressed it should close
 closeModal.addEventListener("click", () => {
-  overlay.classList.toggle("overlay");
-  modalOpen.classList.remove("active");
+  overlay.classList.remove("overlay");
+  webModal.classList.remove("active");
 });
 
-//when clicked outside it also closes
+// x button (DcBot) when pressed it should close
+closeModal2.addEventListener("click", () => {
+  overlay.classList.remove("overlay");
+  dcModal.classList.remove("active");
+});
+
+// when clicked outside it also closes
 overlay.addEventListener("click", () => {
-  overlay.classList.toggle("overlay");
-  modalOpen.classList.remove("active");
+  overlay.classList.remove("overlay");
+  webModal.classList.remove("active");
+  dcModal.classList.remove("active");
 });
