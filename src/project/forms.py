e737ed3ed8e7b7f8fa36db91f19bca0282775543
fredericktulba9@gmail.com <fredericktulba9@gmail.com>
2022-03-06 22:36:57 +0800

    new projects UI design

--- src/project/forms.py
+++ src/project/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class ProjectForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    description = TextAreaField("Description")
-    github_url = StringField("Github URL")
-    site_url = StringField("Site URL")
+    title = StringField("Title", validators=[DataRequired(), Length(max=25)])
+    description = TextAreaField("Description", validators=[Length(max=50)])
+    github_url = StringField("Github URL", validators=[Length(max=50)])
+    site_url = StringField("Site URL", validators=[Length(max=50)])
     image_file = FileField("Image", validators=[FileAllowed(["jpg", "jpeg", "png"])])
     submit = SubmitField("Submit")
